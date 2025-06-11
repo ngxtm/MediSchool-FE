bf2607921c3add879ff148063830044fbc7c4647
Minh Nguyen <177280815+ngxtm@users.noreply.github.com>
2025-06-11 11:24:39 +0700

    Add sub-page for parent

---
+++ src/pages/parent/Vaccination.jsx
@@ -0,0 +1,277 @@
+const Vaccination = () => {
+    return (
+        <div className="flex justify-between gap-4 md:gap-30 md:flex-row flex-col">
+            <div className="md:w-1/2 gap-6 flex flex-col">
+                <h1 className="font-bold text-2xl">Mũi tiêm kế tiếp</h1>
+                <div className="flex flex-row justify-between bg-[#DAEAF7] rounded-xl px-12 py-5">
+                    <div className="flex flex-col gap-4">
+                        <p className="font-bold">CÚM MÙA</p>
+                        <p>15/07/2025</p>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <p className="italic">Đã lên lịch</p>
+                        <a href="#" className="underline">
+                            Xem giấy đồng thuận
+                        </a>
+                    </div>
+                </div>
+                <div className="flex flex-row justify-between bg-[#DAEAF7] rounded-xl px-12 py-5">
+                    <div className="flex flex-col gap-4">
+                        <p className="font-bold">CÚM MÙA</p>
+                        <p>15/07/2025</p>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <p className="italic">Đã lên lịch</p>
+                        <a href="#" className="underline">
+                            Xem giấy đồng thuận
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="md:w-1/2 gap-6 flex flex-col">
+                <h1 className="font-bold text-2xl">Lịch sử tiêm chủng</h1>
+                <div className="flex-1 max-h-[55vh] overflow-y-auto min-h-0 rounded-lg">
+                    <h1 className="bg-[#DAEAF7] px-6 py-3 rounded-xl font-bold">
+                        Phòng bệnh Cúm
+                    </h1>
+                    <div className="flex justify-between">
+                        <div className="px-6 py-3">
+                            <p>Mũi 2</p>
+                            <p>20/08/2024</p>
+                        </div>
+                        <div className="px-6 py-3">
+                            <p>Trường Tiểu học Alphabet</p>
+                            <p className="font-bold">Vaxigrip Tetra 0.5ml</p>
+                        </div>
+                        <div className="flex items-center">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="lucide lucide-chevron-right-icon lucide-chevron-right"
+                            >
+                                <path d="m9 18 6-6-6-6" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="flex justify-between">
+                        <div className="px-6 py-3">
+                            <p>Mũi 2</p>
+                            <p>20/08/2024</p>
+                        </div>
+                        <div className="px-6 py-3">
+                            <p>Trường Tiểu học Alphabet</p>
+                            <p className="font-bold">Vaxigrip Tetra 0.5ml</p>
+                        </div>
+                        <div className="flex items-center">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="lucide lucide-chevron-right-icon lucide-chevron-right"
+                            >
+                                <path d="m9 18 6-6-6-6" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="flex justify-between">
+                        <div className="px-6 py-3">
+                            <p>Mũi 2</p>
+                            <p>20/08/2024</p>
+                        </div>
+                        <div className="px-6 py-3">
+                            <p>Trường Tiểu học Alphabet</p>
+                            <p className="font-bold">Vaxigrip Tetra 0.5ml</p>
+                        </div>
+                        <div className="flex items-center">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="lucide lucide-chevron-right-icon lucide-chevron-right"
+                            >
+                                <path d="m9 18 6-6-6-6" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="flex justify-between">
+                        <div className="px-6 py-3">
+                            <p>Mũi 2</p>
+                            <p>20/08/2024</p>
+                        </div>
+                        <div className="px-6 py-3">
+                            <p>Trường Tiểu học Alphabet</p>
+                            <p className="font-bold">Vaxigrip Tetra 0.5ml</p>
+                        </div>
+                        <div className="flex items-center">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="lucide lucide-chevron-right-icon lucide-chevron-right"
+                            >
+                                <path d="m9 18 6-6-6-6" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="flex justify-between">
+                        <div className="px-6 py-3">
+                            <p>Mũi 2</p>
+                            <p>20/08/2024</p>
+                        </div>
+                        <div className="px-6 py-3">
+                            <p>Trường Tiểu học Alphabet</p>
+                            <p className="font-bold">Vaxigrip Tetra 0.5ml</p>
+                        </div>
+                        <div className="flex items-center">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="lucide lucide-chevron-right-icon lucide-chevron-right"
+                            >
+                                <path d="m9 18 6-6-6-6" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="flex justify-between">
+                        <div className="px-6 py-3">
+                            <p>Mũi 2</p>
+                            <p>20/08/2024</p>
+                        </div>
+                        <div className="px-6 py-3">
+                            <p>Trường Tiểu học Alphabet</p>
+                            <p className="font-bold">Vaxigrip Tetra 0.5ml</p>
+                        </div>
+                        <div className="flex items-center">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="lucide lucide-chevron-right-icon lucide-chevron-right"
+                            >
+                                <path d="m9 18 6-6-6-6" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="flex justify-between">
+                        <div className="px-6 py-3">
+                            <p>Mũi 2</p>
+                            <p>20/08/2024</p>
+                        </div>
+                        <div className="px-6 py-3">
+                            <p>Trường Tiểu học Alphabet</p>
+                            <p className="font-bold">Vaxigrip Tetra 0.5ml</p>
+                        </div>
+                        <div className="flex items-center">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="lucide lucide-chevron-right-icon lucide-chevron-right"
+                            >
+                                <path d="m9 18 6-6-6-6" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="flex justify-between">
+                        <div className="px-6 py-3">
+                            <p>Mũi 2</p>
+                            <p>20/08/2024</p>
+                        </div>
+                        <div className="px-6 py-3">
+                            <p>Trường Tiểu học Alphabet</p>
+                            <p className="font-bold">Vaxigrip Tetra 0.5ml</p>
+                        </div>
+                        <div className="flex items-center">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="lucide lucide-chevron-right-icon lucide-chevron-right"
+                            >
+                                <path d="m9 18 6-6-6-6" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="flex justify-between">
+                        <div className="px-6 py-3">
+                            <p>Mũi 2</p>
+                            <p>20/08/2024</p>
+                        </div>
+                        <div className="px-6 py-3">
+                            <p>Trường Tiểu học Alphabet</p>
+                            <p className="font-bold">Vaxigrip Tetra 0.5ml</p>
+                        </div>
+                        <div className="flex items-center">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="lucide lucide-chevron-right-icon lucide-chevron-right"
+                            >
+                                <path d="m9 18 6-6-6-6" />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Vaccination;

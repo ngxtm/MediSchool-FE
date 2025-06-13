6eea1eae21b68c349ba4430e8119974cc4aa57c4
Minh Nguyen <177280815+ngxtm@users.noreply.github.com>
2025-06-13 22:53:57 +0700

    Show user info on taksbar

--- src/pages/parent/ParentDashboard.jsx
+++ src/pages/parent/ParentDashboard.jsx
@@ -2,11 +2,27 @@ import TaskBar from "../../components/TaskBar";
 import StudentBox from "../../components/StudentBox";
 import NavToggle from "../../components/NavToggle";
 import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import api from "../../utils/api";
 
 const ParentDashboard = () => {
+    const [user, setUser] = useState(null);
+    const fetchUser = async () => {
+        try {
+            const { data } = await api.get('me');
+            setUser(data);
+        } catch (err) {
+            console.error("Error fetching user: ", err);
+        }
+    };
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
     return (
         <div className="min-h-screen bg-gray-50 font-inter">
-            <TaskBar />
+            <TaskBar userData={user}/>
             <div className="px-20 py-6">
                 <StudentBox />
                 <div className="mt-6">

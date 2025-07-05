5c587e17ce4c6aaced62b46e29d30cd4d56bffd7
Minh Nguyen <177280815+ngxtm@users.noreply.github.com>
2025-07-06 02:46:37 +0700

    Enhance UX parent page
    Not allow user back to login when reaching info page (info page is the first page, no history to rollback)

--- src/pages/parent/ParentDashboard.jsx
+++ src/pages/parent/ParentDashboard.jsx
@@ -1,52 +1,53 @@
-import ParentTaskBar from "../../components/ParentTaskBar";
-import StudentBox from "../../components/StudentBox";
-import NavToggle from "../../components/NavToggle";
-import { Outlet } from "react-router-dom";
-import { useEffect, useState } from "react";
-import api from "../../utils/api";
-import { StudentProvider } from "../../context/StudentContext";
-import Loading from "../../components/Loading";
+import ParentTaskBar from '../../components/ParentTaskBar'
+import StudentBox from '../../components/StudentBox'
+import NavToggle from '../../components/NavToggle'
+import { Outlet } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import api from '../../utils/api'
+import { StudentProvider } from '../../context/StudentContext'
+import Loading from '../../components/Loading'
+import useActorNavigation from '../../hooks/useActorNavigation'
 
 const ParentDashboard = () => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+	const [user, setUser] = useState(null)
+	const [loading, setLoading] = useState(true)
 
-    const fetchUser = async () => {
-        try {
-            setLoading(false);
-            const { data } = await api.get('me');
-            setUser(data);
-        } catch (err) {
-            console.error("Error fetching user: ", err);
-        }
-    };
+	useActorNavigation()
 
-    useEffect(() => {
-        fetchUser();
-    }, []);
+	const fetchUser = async () => {
+		try {
+			setLoading(false)
+			const { data } = await api.get('me')
+			setUser(data)
+		} catch (err) {
+			console.error('Error fetching user: ', err)
+		}
+	}
 
-    if (loading) {
-        return (
-            <Loading />
-        )
-    }
+	useEffect(() => {
+		fetchUser()
+	}, [])
 
-    return (
-        <StudentProvider>
-            <div className="min-h-screen font-inter">
-                <ParentTaskBar userData={user}/>
-                <div className="px-20 py-6">
-                    <StudentBox />
-                    <div className="mt-6">
-                        <NavToggle />
-                    </div>
-                    <div className="mt-6">
-                        <Outlet />
-                    </div>
-                </div>
-            </div>
-        </StudentProvider>
-    );
+	if (loading) {
+		return <Loading />
+	}
+
+	return (
+		<StudentProvider>
+			<div className="min-h-screen font-inter">
+				<ParentTaskBar userData={user} />
+				<div className="px-20 py-6">
+					<StudentBox />
+					<div className="mt-6">
+						<NavToggle />
+					</div>
+					<div className="mt-6">
+						<Outlet />
+					</div>
+				</div>
+			</div>
+		</StudentProvider>
+	)
 }
 
-export default ParentDashboard;
+export default ParentDashboard

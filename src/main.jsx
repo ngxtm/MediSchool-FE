42369c77eadc2255c9b801ecb103acaf9ada955b
Minh Nguyen <177280815+ngxtm@users.noreply.github.com>
2025-07-11 21:47:59 +0700

    Improves application performance monitoring
    Adds Vercel Speed Insights for performance monitoring.
    Refactors the download template logic in the import excel modal to use the api instance.
    Updates API base URL to use environment variable for better configuration.

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,23 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "../src/styles/index.css";
-import "react-toastify/dist/ReactToastify.css";
-import { AuthContextProvider } from "./context/AuthContext";
-import { RouterProvider } from "react-router-dom";
-import router from "./utils/router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ToastContainer } from "react-toastify";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '../src/styles/index.css'
+import 'react-toastify/dist/ReactToastify.css'
+import { AuthContextProvider } from './context/AuthContext'
+import { RouterProvider } from 'react-router-dom'
+import router from './utils/router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ToastContainer } from 'react-toastify'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+const queryClient = new QueryClient()
 
-const queryClient = new QueryClient();
-
-createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<AuthContextProvider>
-				<RouterProvider router={router} />
-				<ToastContainer />
-			</AuthContextProvider>
-		</QueryClientProvider>
-	</StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <SpeedInsights />
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <RouterProvider router={router} />
+        <ToastContainer />
+      </AuthContextProvider>
+    </QueryClientProvider>
+  </StrictMode>
+)

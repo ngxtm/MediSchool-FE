aee866f9978e427f65cc35718ca0e12cb540a3dd
Minh Nguyen <177280815+ngxtm@users.noreply.github.com>
2025-07-28 16:10:03 +0700

    Remove unused import

--- src/pages/manager/Manager.jsx
+++ src/pages/manager/Manager.jsx
@@ -3,44 +3,43 @@ import ManagerTaskBar from './components/ManagerTaskbar'
 import { useState } from 'react'
 import api from '../../utils/api'
 import { useEffect } from 'react'
-import { Cardio } from 'ldrs/react'
 import useActorNavigation from '../../hooks/useActorNavigation'
 import Loading from '../../components/Loading'
 
 const Manager = () => {
-	const [user, setUser] = useState(null)
-	const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
 
-	useActorNavigation('manager')
+  useActorNavigation('manager')
 
-	const fetchUser = async () => {
-		try {
-			setLoading(false)
-			const { data } = await api.get('/me')
-			setUser(data)
-		} catch (err) {
-			console.error('Error fetching user: ', err)
-		}
-	}
+  const fetchUser = async () => {
+    try {
+      setLoading(false)
+      const { data } = await api.get('/me')
+      setUser(data)
+    } catch (err) {
+      console.error('Error fetching user: ', err)
+    }
+  }
 
-	useEffect(() => {
-		fetchUser()
-	}, [])
+  useEffect(() => {
+    fetchUser()
+  }, [])
 
-	if (loading) {
-		return <Loading />
-	}
+  if (loading) {
+    return <Loading />
+  }
 
-	return (
-		<div className="min-h-screen bg-[#eefdf8]">
-			<ManagerTaskBar userData={user} />
-			<div className="p-4">
-				<div className="px-28 pt-5">
-					<Outlet />
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className="min-h-screen bg-[#eefdf8]">
+      <ManagerTaskBar userData={user} />
+      <div className="p-4">
+        <div className="px-28 pt-5">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Manager
